82ca50f6c65f086a20102817010a2d7af10713eb
Seonghyeon Kim <kim@seonghyeon.dev>
2020-02-09 18:53:15 +0900

    feat: Using bind

--- main.go
+++ main.go
@@ -5,9 +5,22 @@ import (
 	"net/http"
 )
 
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
 func getUser(c echo.Context) error {
 	id := c.Param("id")
 	return c.String(http.StatusOK, id)
+}
+
+func createUser(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, u)
 }
 
 func main() {
@@ -17,6 +30,7 @@ func main() {
 		return c.String(http.StatusOK, "Hello Gophers!")
 	})
 	e.GET("/users/:id", getUser)
+	e.POST("/users/:id", createUser)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
